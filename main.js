387b5c4534e7c0f3d9a7aaa29ff5b5e29418ed49
ehdeneve <elwyn_de_neve@hotmail.com>
2022-02-16 10:00:49 +0100

    antwoorden

--- main.js
+++ main.js
@@ -74,3 +74,31 @@ Er zijn meerdere wegen naar Rome, dus als jij denkt dat er een efficiëntere man
 ** gebruik hiervoor een Array methode
 
 */
+
+function createArray( lengthArray ) {
+    const array = [];
+    for ( let i = 1; i <= lengthArray; i++ ) {
+        array.push( i );
+    }
+    return array;
+}
+
+// console.log(createArray(70));
+
+function getJuf( param ) {
+    const replaceWithJuf = createArray( param );
+    //return lengthArray
+    for ( let i = 0; i < replaceWithJuf.length; i++ ) {
+        if ( replaceWithJuf[i] % 7 === 0 ) {
+            replaceWithJuf[i] = 'juf'
+        } else {
+            let digitToString = replaceWithJuf[i].toString()
+            if (digitToString.includes(7)) {
+                replaceWithJuf[i] = 'juf'
+            }
+        }
+    }
+    return replaceWithJuf
+}
+
+console.log( getJuf( 70 ) );
